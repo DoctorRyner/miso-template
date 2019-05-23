eb57c72fda5a16d06f47021e02042967706a549b
DoctorRyner <drryner@icloud.com>
2019-05-24 04:34:04 +0500

    First working examples

--- src/Main.hs
+++ src/Main.hs
@@ -9,6 +9,7 @@ import           Types
 import qualified Miso.Styled.Html as Styled
 import           View.Header
 import           View.TODO
+import Clay
 
 update' :: Event -> Model -> Effect Event Model
 update' event model = case event of
@@ -16,16 +17,30 @@ update' event model = case event of
     Inc     -> noEff $ model { counter = succ model.counter }
 
 view' :: Model -> View Event
-view' model = div_ []
+view' _model = div_ []
     -- [ Style.Global.css
     -- , View.Header.render model
     -- , View.TODO.render model
     -- ]
     [ Styled.toUnstyled $ Styled.node "div" []
         [ Styled.text "testa"
-        , Styled.node "h1" [ Styled.class_ "tek" ] [ Styled.text "keka" ]
+        , title [] [ Styled.text "Styled title another exemple" ]
+        , Styled.node "h1" [ Styled.class_ "tek" ] [ Styled.text "kek" ]
+        , title [] [ Styled.text "Styled title" ]
+        , btn [] [ Styled.text "Click me" ]
         ]
     ]
+  where
+    title = Styled.styled "h1" "title" $ do
+        color "#e32"
+
+    btn = Styled.styled "div" "btn" $ do
+        border solid (px 2) "#000000"
+        borderRadius (px 1) (px 1) (px 1) (px 1)
+        height $ px 50
+        width $ px 150
+
+        hover & do transform $ scale 1.2 1.2
 
 main :: IO ()
 main = do
--- src/Miso/Styled/Html.hs
+++ src/Miso/Styled/Html.hs
@@ -2,12 +2,14 @@ module Miso.Styled.Html where
 
 import qualified Miso
 import Miso.String hiding (map)
--- import Clay hiding (map)
+import Clay hiding (map)
+import qualified Css.Extra
+import qualified Data.Text as Text
 
 mshow :: ToMisoString a => a -> MisoString
 mshow = Miso.String.ms
 
-data VTree a = VNode String [Attribute a] [VTree a] | VText String
+data VTree a = VNode (String, Css) String [Attribute a] [VTree a] | VText String
 
 data Attribute a = Attribute (Miso.Attribute a)
 data View a = View (Miso.View a)
@@ -25,14 +27,50 @@ text :: String -> VTree a
 text = VText
 
 node :: String -> [Attribute a] -> [VTree a] -> VTree a
-node = VNode
+node = VNode ("", pure ())
+
+styled :: String -> String -> Css -> [Attribute a] -> [VTree a] -> VTree a
+styled tag cssTag cssCode = VNode (cssTag, cssCode) tag
+
+collectHtml :: VTree a -> Miso.View a
+collectHtml (VText str) = Miso.text $ mshow str
+collectHtml (VNode (cssTag, _) tag attrs childs) = Miso.nodeHtml
+    (mshow tag)
+    (map toUnstyledAttribute attrs ++ [ Miso.class_ $ mshow cssTag ])
+    (map collectHtml childs)
+
+collectCss :: VTree a -> [(String, Css)] -> [(String, Css)]
+collectCss (VText _) store = store ++ [("", pure ())]
+collectCss (VNode cssToCollect _ _ childs) store = mconcat
+    [ store
+    , [cssToCollect]
+    , mconcat $ map (\child -> collectCss child store) childs
+    ]
+
+isCssTagUnique :: String -> [(String, Css)] -> Bool
+isCssTagUnique _ [] = True
+isCssTagUnique cssTag ((cssTagInList, _):xs) = if cssTag == cssTagInList
+    then False
+    else isCssTagUnique cssTag xs
+
+generateCss :: [(String, Css)] -> [(String, Css)] -> Miso.View a
+generateCss [] styles = Miso.nodeHtml "style" []
+    [ Miso.text $ mshow $ render $ Prelude.foldr
+        (\(cssTag, cssCode) res -> res <> (Css.Extra.class' (Text.pack cssTag) cssCode) )
+        (pure ())
+        styles
+    ]
+generateCss ((cssTag, cssCode):xs) styles = if isCssTagUnique cssTag styles
+    then generateCss xs (styles ++ [(cssTag, cssCode)])
+    else generateCss xs styles
+-- generateCss ((cssTag, cssCode):xs) styles = if 
 
 toUnstyled :: VTree a -> Miso.View a
-toUnstyled (VText str) = Miso.text $ mshow str
-toUnstyled (VNode tag attrs childs) = Miso.nodeHtml
-    (mshow tag)
-    (map toUnstyledAttribute attrs)
-    (map toUnstyled childs)
+toUnstyled styledTree = Miso.div_ []
+    [ Miso.text "Alpha Styled Elements Generation"
+    , generateCss (collectCss styledTree []) []
+    , collectHtml styledTree
+    ]
 
 class_ :: String -> Attribute a
 class_ = Attribute . Miso.class_ . mshow
